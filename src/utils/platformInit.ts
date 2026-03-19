5481b3a050cffd406492fe0a8ec2023d6580dffb
Matteo Burgassi <matteo.burgassi@digitalvirgo.com>
2026-03-19 11:35:42 +0100

    added platform on weebview

--- src/utils/platformInit.ts
+++ src/utils/platformInit.ts
@@ -13,8 +13,9 @@ export function platformInit() {
 }
 
 export function isTV(): boolean {
+  if ((window as any).__TV_PLATFORM__) return true;
   if (window.tizen) return true;
   if (window.webOS) return true;
-  if (/Android/.test(navigator.userAgent) && /TV|AFT|BRAVIA|MIBOX/.test(navigator.userAgent)) return true;
+  if (/Android/.test(navigator.userAgent) && /TV|AFT|BRAVIA|MIBOX|Chromecast/.test(navigator.userAgent)) return true;
   return false;
 }

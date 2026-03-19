77cb51a9da82c4f691156ae88b8ac1cbdd2f820f
mar-031 <marina.mekail@digitalvirgo.com>
2026-03-19 10:17:23 +0100

    Update LoginPage for platform-specific inputs

--- src/utils/platformInit.ts
+++ src/utils/platformInit.ts
@@ -11,3 +11,10 @@ export function platformInit() {
     keys.forEach((key) => window.tizen!.tvinputdevice.registerKey(key));
   }
 }
+
+export function isTV(): boolean {
+  if (window.tizen) return true;
+  if (window.webOS) return true;
+  if (/Android/.test(navigator.userAgent) && /TV|AFT|BRAVIA|MIBOX/.test(navigator.userAgent)) return true;
+  return false;
+}
